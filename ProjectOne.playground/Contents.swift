3eb31491e5a5d90323e0ca5e4552f07324029f4a
David Kaneshiro <davidk9@hawaii.edu>
2018-02-05 11:28:00 -1000

    Finished Variable Coding Challenge #1

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 var str = "Hello, playground"
+//Variable Coding Challenge #1
+var firstString: String
+firstString = "I Love"
+let secondString: String
+secondString = "Anime"
+
+firstString+secondString
+
+var thirdString = firstString+secondString
+print(thirdString)
+
+//Variable Coding Challenge #2
+
+let celsius: float
+let fahrenheit: float = 50
